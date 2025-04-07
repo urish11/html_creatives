375dd72d23ce7e56ecc2562c92feda11cd729e45
urish11 <117926571+urish11@users.noreply.github.com>
2025-04-07 08:08:39 +0000

    Refine gemini prompt generation to encourage more creative and varied image ideas

--- app.py
+++ app.py
@@ -1277,7 +1277,7 @@ if st.button("Generate Images"):
                     
                     if template_str == 'gemini7batch': # gemini1 with geimini text
                         gemini_prompt = gemini_text_lib(f"""write short prompt for\ngenerate square image promoting '{topic}' in language {lang} . add a CTA button with 
-                                                'Learn More Here >>' in appropriate language\ \nshould be low quality and very enticing and alerting \n\nstart with 'square image aspect ratio of 1:1 of '\n\n be specific in what is shown . return JUST the {count} best options, each prompt is a FULL PROMPT !! each at least 500 chrs(dont write it),be creative and have variance between the prompts, no intros , as json key is int index , it's value is the prompt. first barinstrom deeply!
+                                                'Learn More Here >>' in appropriate language\ \nshould be low quality and very enticing and alerting \n\nstart with 'square image aspect ratio of 1:1 of '\n\n be specific in what is shown . return JUST the {count} best options, each prompt is a FULL PROMPT !! each at least 500 chrs(dont write it),be creative and have variance between the prompts, no intros , as json key is int index , it's value is the prompt. first barinstrom deeply at least {count*3} ideas.
 
                             """)
                         gemini_prompt= gemini_prompt.replace('```json','').replace("```","").replace("python","")
