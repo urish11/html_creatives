0bb9f626d830c4f47b22703c04c4cd0f1003d99c
urish11 <117926571+urish11@users.noreply.github.com>
2025-04-26 12:25:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1464,11 +1464,11 @@ if st.button("Generate Images"):
                         if cache_key in st.session_state and st.session_state[cache_key]:
                             st.text("using cached prompt")
                             gemini_prompt = st.session_state[cache_key]
-                    else:
-                        gemini_prompt = gemini_text_lib(f"""write short prompt for\ngenerate square image promoting '{topic}' in language {lang} . add a CTA button with 
-                                                'Learn More Here >>' in appropriate language\\nand 'act fast' or 'limited available' \n \nshould be low quality and very enticing and alerting \n\nstart with 'square image aspect ratio of 1:1 of '\n\n be specific in what is shown . return JUST the best option, no intros
-                        """)
-                        st.session_state[cache_key] = gemini_prompt
+                        else:
+                            gemini_prompt = gemini_text_lib(f"""write short prompt for\ngenerate square image promoting '{topic}' in language {lang} . add a CTA button with 
+                                                    'Learn More Here >>' in appropriate language\\nand 'act fast' or 'limited available' \n \nshould be low quality and very enticing and alerting \n\nstart with 'square image aspect ratio of 1:1 of '\n\n be specific in what is shown . return JUST the best option, no intros
+                            """)
+                            st.session_state[cache_key] = gemini_prompt
 
                     
                     if template_str == 'gemini7claude': # gemini1 with geimini text
