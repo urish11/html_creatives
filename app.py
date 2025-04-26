901f2436ddcadff76db2d7dac8fc62f8860abad2
urish11 <117926571+urish11@users.noreply.github.com>
2025-04-26 13:03:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1469,7 +1469,7 @@ if st.button("Generate Images"):
                             st.session_state[cache_key]["count"] = st.session_state[cache_key]["count"] +1
                         else:
                             gemini_prompt = gemini_text_lib(f"""write short prompt for\ngenerate square image promoting '{topic}' in language {lang} . add a CTA button with 
-                                                    'Learn More Here >>' in appropriate language\\nand 'act fast' or 'limited available' \n \nshould be low quality and very enticing and alerting \n\nstart with 'square image aspect ratio of 1:1 of '\n\n be specific in what is shown . return JUST the best option, no intros
+                                                    'Learn More Here >>' in appropriate language\\nand 'act fast' or 'limited available' \n \nshould be low quality and very enticing and alerting!! \n\nstart with 'square image aspect ratio of 1:1 of '\n\n be specific in what is shown . return JUST the best option, no intros
                             """)
                             st.session_state[cache_key]["data"] = gemini_prompt
                             st.session_state[cache_key]["count"] = st.session_state[cache_key]["count"] +1
