cc495a4588910d1f19c5e7a148938b9422f5d402
urish11 <117926571+urish11@users.noreply.github.com>
2025-06-26 12:32:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1490,6 +1490,8 @@ if st.button("🚀 Start Phase 1: Generate Raw Images (Queued)", type="primary",
 
 # Phase 1 Processing Loop
 if st.session_state.get('img_gen_processing_active') and st.session_state.img_gen_task_queue:
+    st.info(f"Phase 1: Processing task {current_idx + 1}/{total_tasks} - Topic: '{task_to_process['original_topic']}', Template: '{task_to_process['chosen_template']}' Trial : {trial}")
+
     with st.status("Proccessing Log:"):
     
        trial = 1
@@ -1497,7 +1499,6 @@ if st.session_state.get('img_gen_processing_active') and st.session_state.img_ge
        current_idx = st.session_state.img_gen_current_task_idx
        total_tasks = st.session_state.img_gen_total_tasks
        img_url_result = None
-       st.info(f"Phase 1: Processing task {current_idx + 1}/{total_tasks} - Topic: '{task_to_process['original_topic']}', Template: '{task_to_process['chosen_template']}' Trial : {trial}")
        st.progress((current_idx) / total_tasks if total_tasks > 0 else 0)
        while img_url_result is None and trial < 5 :
            try:
