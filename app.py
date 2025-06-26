58d7fee3bf331d2a741fd559c2a2769be9a228fd
urish11 <117926571+urish11@users.noreply.github.com>
2025-06-26 19:17:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def google_sheets_append_df(spreadsheet_id,range_name, df_data_input ):
     service = build("sheets", "v4", credentials=creds)
     time_now = [datetime.now(ZoneInfo("Asia/Jerusalem")).strftime("%Y-%m-%d %H:%M")]
     df_list = df_data_input.values.tolist()
-    for idx in len(df_list):
+    for idx in range(len(df_list)):
         df_list[idx] = [time_now] + df_list[idx]
         
     
